8e5baa7c8c6f8dc460243eb32ad3a63969a9d355
MDRobiulHassan <robiul110256@gmail.com>
2026-08-18 21:36:48 +0600

    Custom hooks

---
+++ React/custom-hooks/src/data/products.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    name: "Wireless Headphones",
+    price: 99,
+  },
+  {
+    id: 2,
+    name: "Wireless Mouse",
+    price: 49,
+  },
+  {
+    id: 3,
+    name: "Mechanical Keyboard",
+    price: 129,
+  },
+  {
+    id: 4,
+    name: "USB-C Hub",
+    price: 39,
+  },
+  {
+    id: 5,
+    name: "Bluetooth Speaker",
+    price: 79,
+  },
+  {
+    id: 6,
+    name: "Webcam",
+    price: 89,
+  },
+];
