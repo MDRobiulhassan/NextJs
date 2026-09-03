e56c3720002245b5e63443222b3d643f8db576d5
MDRobiulHassan <robiul110256@gmail.com>
2026-09-03 21:10:53 +0600

    State Management - Zustand

---
+++ React/state-management-zustand/src/store/postsStore.js
@@ -0,0 +1,20 @@
+import { create } from "zustand";
+
+export const usePostsStore = create((set) => ({
+  posts: [],
+  loading: false,
+  error: null,
+
+  fetchPosts: async () => {
+    set({ loading: true, error: null });
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts",
+      );
+      const data = await response.json();
+      set({ posts: data, loading: false });
+    } catch (error) {
+      set({ error: error.message, loading: false });
+    }
+  },
+}));
